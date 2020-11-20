0bbca756d7c66e2f612d6cecd70d50a73d5acb2b
priyadrsnipd <priya.padman@anywhere.co>
2020-11-20 13:43:02 +0530

    Import images in index.js file

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './scss/style.scss';
 import './js/app.js';
+import mainIllustration from './images/main-illustration.png';
+import sti from './images/short-term-investment.jpg';
+import lti from './images/long-term-investment.jpg';
